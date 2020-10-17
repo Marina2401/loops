8be99f418ef8a66cfe40e559f30bcab4a922dae1
Marina <farba2105@inbox.ru>
2020-10-17 16:14:46 +0300

    еще несколько задач

---
+++ 170820, Lists/lr_dvumernie_spiski/13/13.1.py
@@ -0,0 +1,38 @@
+#13.1. Определить наименьший элемент каждой четной строки матрицы А[М, N].
+
+import random
+
+def min(a, n, m):
+
+
+    d = []
+    for i in range(0, n, 2):
+        mi = 20
+        c = []
+        for j in range(m):
+            if a[i][j] < mi:
+                mi = a[i][j]
+        print(mi)
+    return True
+
+def main():
+    n = int(input('Введите количество строк матрицы '))
+    m = int(input('введите количество столбцов '))
+    a = []
+    for i in range(n):
+        b = []
+        for j in range(m):
+            b.append(random.randint(-10, 10))
+        a.append(b)
+
+    for i in range(n):
+        for j in range(m):
+            print(a[i][j], end=' ')
+        print()
+    print()
+
+    min(a, n, m)
+
+
+
+main()
---
+++ 170820, Lists/lr_dvumernie_spiski/13/13.2.py
@@ -0,0 +1,51 @@
+#13.2 Найти наибольший и наименьший элементы прямоугольной матрицы и поменять их местами.
+
+import random
+
+def change(a, n):
+    ma = -20
+    i_ma = 0
+    j_ma = 0
+    for i in range(n):
+        for j in range(n):
+            if a[i][j] > ma:
+                ma = a[i][j]
+                i_ma = i
+                j_ma = j
+
+    mi = 20
+    i_mi = 0
+    j_mi = 0
+    for i in range(n):
+        for j in range(n):
+            if a[i][j] < mi:
+                mi = a[i][j]
+                i_mi = i
+                j_mi = j
+    a[i_mi][j_mi], a[i_ma][j_ma] = a[i_ma][j_ma], a[i_mi][j_mi]
+
+    return a
+
+def main():
+    n = int(input('Введите порядок матрицы '))
+    a = []
+    for i in range(n):
+        b = []
+        for j in range(n):
+            b.append(random.randint(0, 9))
+        a.append(b)
+
+    for i in range(n):
+        for j in range(n):
+            print(a[i][j], end=' ')
+        print()
+    print()
+
+    c = change(a, n)
+    for i in range(n):
+        for j in range(n):
+            print(a[i][j], end=' ')
+        print()
+    print()
+
+main()
